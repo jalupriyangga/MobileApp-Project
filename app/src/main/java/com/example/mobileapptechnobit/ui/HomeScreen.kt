71c8244f2d536e92e47008e2d6ed4bf38426c4cc
Hironemus Apriliano <hironemus.apriliano@gmail.com>
2025-05-08 21:03:22 +0700

    present menu dialog

--- app/src/main/java/com/example/mobileapptechnobit/ui/HomeScreen.kt
+++ app/src/main/java/com/example/mobileapptechnobit/ui/HomeScreen.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -22,8 +23,11 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
@@ -39,14 +43,18 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import androidx.compose.ui.zIndex
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
@@ -64,6 +72,7 @@ import com.example.mobileapptechnobit.data.repository.AuthRepository
 import com.example.mobileapptechnobit.data.repository.ProfileRepository
 import com.example.mobileapptechnobit.ui.component.BottomNavigationBar
 import com.example.mobileapptechnobit.ui.component.TopAppBar
+import com.example.mobileapptechnobit.ui.theme.primary100
 import com.example.mobileapptechnobit.ui.theme.robotoFontFamily
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -85,7 +94,10 @@ fun HomeScreen(modifier: Modifier = Modifier, navCtrl: NavController) {
             BottomNavigationBar(navCtrl = navCtrl, index = 0)
         },
     ){ padding ->
-        Column (Modifier.padding(padding).fillMaxSize().zIndex(1f))
+        Column (Modifier
+            .padding(padding)
+            .fillMaxSize()
+            .zIndex(1f))
         {
             MainMenu(navCtrl = navCtrl)
         }
@@ -158,7 +170,9 @@ fun ScheduleCard(modifier: Modifier = Modifier, navCtrl: NavController) {
                 Spacer(Modifier.weight(1f))
                 Text("Lihat Jadwal", color = Color.Gray, fontSize = 14.sp, modifier = Modifier.clickable{navCtrl.navigate("schedule_screen")})
                 Spacer(Modifier.padding(start = 10.dp))
-                Icon(imageVector = Icons.Default.ArrowForward, contentDescription = null, Modifier.size(20.dp).clickable{navCtrl.navigate("schedule_screen")})
+                Icon(imageVector = Icons.Default.ArrowForward, contentDescription = null, Modifier
+                    .size(20.dp)
+                    .clickable { navCtrl.navigate("schedule_screen") })
             }
             Spacer(modifier = Modifier.height(10.dp))
             HorizontalDivider(
@@ -209,9 +223,15 @@ fun ScheduleCard(modifier: Modifier = Modifier, navCtrl: NavController) {
 
 @Composable
 fun MainMenu(modifier: Modifier = Modifier, navCtrl: NavController, viewModel: CameraPresViewModel = remember { CameraPresViewModel() }) {
+
+    var showPresentDialog by remember { mutableStateOf(false) }
     val context = LocalContext.current
+
     Column(
-        modifier = Modifier.padding(horizontal = 16.dp).padding(top = 20.dp).padding(bottom = 10.dp)
+        modifier = Modifier
+            .padding(horizontal = 16.dp)
+            .padding(top = 20.dp)
+            .padding(bottom = 10.dp)
     ) {
         Text("Menu Utama", fontWeight = FontWeight.Bold, fontSize = 25.sp, fontFamily = robotoFontFamily)
         Spacer(modifier = Modifier.height(8.dp))
@@ -222,17 +242,26 @@ fun MainMenu(modifier: Modifier = Modifier, navCtrl: NavController, viewModel: C
             MenuItem(
                 painter = painterResource(R.drawable.presensi),
                 label = "Presensi",
-                onClick = {
-                    // Cek apakah pengguna sudah melakukan Clock In
-                    val clockInTime = viewModel.getClockInTime(context)
-                    if (clockInTime > 0L) {
-                        // Jika sudah Clock In tetapi belum Clock Out, navigasi ke ClockOutScreen
-                        navCtrl.navigate(Screen.ClockOut.route)
-                    } else {
-                        // Jika belum Clock In, navigasi ke CameraPresensiScreen
-                        navCtrl.navigate(Screen.CameraPresensi.route)
-                    }
-                })
+
+                onClick = { showPresentDialog = true },
+
+//                onClick = {
+//                    // Cek apakah pengguna sudah melakukan Clock In
+//                    val clockInTime = viewModel.getClockInTime(context)
+//                    if (clockInTime > 0L) {
+//                        // Jika sudah Clock In tetapi belum Clock Out, navigasi ke ClockOutScreen
+//                        navCtrl.navigate(Screen.ClockOut.route)
+//                    } else {
+//                        // Jika belum Clock In, navigasi ke CameraPresensiScreen
+//                        navCtrl.navigate(Screen.CameraPresensi.route)
+//                    }
+//                }
+            )
+            PresentMenuDialog(
+                showDialog = showPresentDialog,
+                navCtrl = navCtrl,
+                onDismiss = { showPresentDialog = false}
+            )
             MenuItem(
                 painter = painterResource(R.drawable.patroli),
                 label = "Patroli",
@@ -253,7 +282,9 @@ fun MenuItem(modifier: Modifier = Modifier, painter: Painter, label: String, onC
         colors = CardDefaults.cardColors(Color.White)
     ) {
         Column(
-            modifier = Modifier.padding(16.dp).fillMaxWidth(),
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
@@ -261,6 +292,96 @@ fun MenuItem(modifier: Modifier = Modifier, painter: Painter, label: String, onC
             Spacer(modifier = Modifier.height(8.dp))
             Text(label, fontFamily = robotoFontFamily, fontWeight = FontWeight(400), fontSize = 17.sp)
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PresentMenuDialog(modifier: Modifier = Modifier, showDialog: Boolean, navCtrl: NavController, onDismiss: () -> Unit, viewModel: CameraPresViewModel = remember { CameraPresViewModel() }) {
+
+    val context = LocalContext.current
+    if(showDialog){
+
+        Dialog(
+            onDismissRequest = { onDismiss() },
+            properties = DialogProperties(),
+            content = {
+                Box(
+                    modifier.fillMaxWidth().background(
+                        color = Color.White,
+                        shape = RoundedCornerShape(10.dp)
+                    )
+                ){
+                    Column (modifier.fillMaxWidth().padding(horizontal = 20.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+                        Text(text = "Pilih aksi", fontFamily = robotoFontFamily, fontWeight = FontWeight(500), fontSize = 20.sp, modifier = Modifier.padding(vertical = 20.dp))
+                        Column (
+                            Modifier
+                                .border(width = 1.dp, shape = RoundedCornerShape(1.dp), color = Color.LightGray)
+                                .fillMaxWidth()
+                                .clickable {
+                                    // Cek apakah pengguna sudah melakukan Clock In
+                                    val clockInTime = viewModel.getClockInTime(context)
+                                    if (clockInTime > 0L) {
+                                    // Jika sudah Clock In tetapi belum Clock Out, navigasi ke ClockOutScreen
+                                         navCtrl.navigate(Screen.ClockOut.route)
+                                    } else {
+                                     // Jika belum Clock In, navigasi ke CameraPresensiScreen
+                                         navCtrl.navigate(Screen.CameraPresensi.route)
+                                    }
+                                }
+                        )
+                        {
+                            Row (verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Start, modifier = Modifier.padding(horizontal = 20.dp, vertical = 10.dp)) {
+                                Image(painter = painterResource(R.drawable.frame_8748), contentDescription = null, Modifier.size(40.dp))
+                                Spacer(Modifier.padding(horizontal = 5.dp))
+                                Column {
+                                    Text(text = "Presensi", fontFamily = robotoFontFamily, fontWeight = FontWeight(500), fontSize = 15.sp)
+                                    Text(text = "Lakukan presensi hari ini", fontFamily = robotoFontFamily, fontWeight = FontWeight(400), fontSize = 13.sp)
+                                }
+                            }
+                        }
+                        Spacer(Modifier.padding(5.dp))
+                        Column (Modifier.border(width = 1.dp, shape = RoundedCornerShape(1.dp), color = Color.LightGray).fillMaxWidth()){
+                            Row (verticalAlignment = Alignment.CenterVertically,horizontalArrangement = Arrangement.Start, modifier = Modifier.padding(horizontal = 20.dp, vertical = 10.dp)) {
+                                Image(painter = painterResource(R.drawable.kuning), contentDescription = null, Modifier.size(40.dp))
+                                Spacer(Modifier.padding(horizontal = 5.dp))
+                                Column {
+                                    Text(text = "Perizinan", fontFamily = robotoFontFamily, fontWeight = FontWeight(500), fontSize = 15.sp)
+                                    Text(text = "Ajukan izin tidak masuk kerja", fontFamily = robotoFontFamily, fontWeight = FontWeight(400), fontSize = 13.sp)
+                                }
+                            }
+                        }
+                        HorizontalDivider(Modifier.padding(vertical = 20.dp))
+
+                        Text(text = "Tutup", fontFamily = robotoFontFamily, fontWeight = FontWeight(400), fontSize = 17.sp, color = primary100, modifier = Modifier.padding(bottom = 20.dp).clickable { onDismiss() })
+                    }
+                }
+            },
+        )
+
+//        AlertDialog(
+//            onDismissRequest = { onDismiss() },
+//            title = {
+//                Text(
+//                    text = "Pilih aksi",
+//                    fontSize = 16.sp,
+//                    fontWeight = FontWeight.Bold,
+//                    fontFamily = robotoFontFamily
+//                )
+//            },
+//
+//            confirmButton = {
+//                Button(
+//                    onClick = { onDismiss() }
+//                ) {
+//                    Text(text = "Tutup")
+//                }
+//            },
+//            containerColor = Color.White,
+//            shape = RoundedCornerShape(16.dp),
+//            modifier = Modifier.background(Color.Transparent)
+//        )
+
     }
 }
 
