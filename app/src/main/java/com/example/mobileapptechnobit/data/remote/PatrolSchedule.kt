844a180bb16475ff88190cf85b0662fd9a6ef656
SandroChrist <sibueasandro@gmail.com>
2025-06-20 10:21:00 +0700

    Update PatrolSchedule.kt

--- app/src/main/java/com/example/mobileapptechnobit/data/remote/PatrolSchedule.kt
+++ app/src/main/java/com/example/mobileapptechnobit/data/remote/PatrolSchedule.kt
@@ -1,10 +1,14 @@
 package com.example.mobileapptechnobit.data.remote
 
+data class PatrolScheduleWrapper(
+    val success: Boolean,
+    val data: List<PatrolScheduleResponse>
+)
 data class PatrolScheduleResponse(
     val tanggal: String,
-    val waktu: String,
-    val jam_mulai: String,
-    val jam_selesai: String,
-    val lokasi: String,
-    val rekan_tugas: List<String>
+    val waktu: String?,
+    val jam_mulai: String?,
+    val jam_selesai: String?,
+    val lokasi: String?,
+    val rekan_tugas: String?
 )
